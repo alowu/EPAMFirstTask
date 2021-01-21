50a8f4c38479ff01e74079ff5ac1de42f6e472e1
alo_wu <ilya.bela@yandex.ru>
2021-01-21 14:19:32 +0300

    add: getElement, equals methods

--- src/main/java/com/epam/task/first/entities/Array.java
+++ src/main/java/com/epam/task/first/entities/Array.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Array {
 
-    private final List<Integer> elements;
+    private List<Integer> elements;
 
     public Array(Integer... elements) {
         this.elements = new ArrayList<>();
@@ -19,6 +19,32 @@ public class Array {
 
     public List<Integer> getElements(){
         return elements;
+    }
+
+    public Integer getElement(int index) {
+        return this.elements.get(index);
+    }
+
+    public void setElements(List<Integer> data) {
+        this.elements = data;
+    }
+
+    public int length() {
+        int len = 0;
+        for (Integer element : this.elements){
+            ++len;
+        }
+        return len;
+    }
+
+    public boolean equals(Array array) {
+        int arrayLen = array.length();
+        for (int i = 0; i < arrayLen; ++i){
+            if (this.getElement(i).intValue() != array.getElement(i).intValue()) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
